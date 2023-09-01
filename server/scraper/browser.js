aec533d32ea79e242b26416896ef51ed371fe7a1
Mary Jiang <mvjiang@gmail.com>
2023-09-01 00:04:01 -0400

    updated args and executable path for Pupeeteer

--- server/scraper/browser.js
+++ server/scraper/browser.js
@@ -1,10 +1,18 @@
 const puppeteer = require('puppeteer');
+require("dotenv").config();
 
 async function startBrowser(){
 	let browser;
 	try {
 	    console.log("Opening the browser......");
 	    browser = await puppeteer.launch({
+			args: [
+				"--disable-setuid-sandbox",
+				"--no-sandbox",
+				"--single-process",
+				"--no-zygote",
+			],
+			executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH,
 	        headless: true,
 	        args: ["--disable-setuid-sandbox"],
 	        'ignoreHTTPSErrors': true
