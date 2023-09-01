1476ef58504d1e34700e3ab760df0490e266df9b
Mary Jiang <mvjiang@gmail.com>
2023-09-01 13:00:31 -0400

    Update browser.js

--- server/scraper/browser.js
+++ server/scraper/browser.js
@@ -12,7 +12,10 @@ async function startBrowser(){
 				"--single-process",
 				"--no-zygote",
 			],
-			executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH,
+			executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
 	        headless: true,
 	        args: ["--disable-setuid-sandbox"],
 	        'ignoreHTTPSErrors': true
